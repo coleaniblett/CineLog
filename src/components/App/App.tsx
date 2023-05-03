bb1fe00c123e4eb95ecb606f1a254d8cc0678fa5
ColeANiblett <coleaniblett@gmail.com>
2023-05-03 15:11:21 -0700

    set up routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Signup } from '../Signup/Signup';
+import { LogIn } from '../LogIn/LogIn';
+import { Home } from '../Home/Home';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../../contexts/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <AuthProvider>
-      <Container 
-        className="d-flex align-items-center justify-content-center" 
-        style={{ minHeight: "100vh"}}
-      >
-        <div className="w=100" style={{ maxWidth: 400}}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
-    );
+    <Container 
+      className="d-flex align-items-center justify-content-center" 
+      style={{ minHeight: "100vh"}}
+    >
+      <div className="w=100" style={{ maxWidth: 400}}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              {/*Make 'exact' path?*/}
+              <Route path="/" element={<LogIn />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/LogIn/LogIn.tsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../../contexts/AuthContext';
+
+export const LogIn: React.FC = () => {
+  const emailRef = useRef<HTMLInputElement>(document.createElement("input"));
+  const passwordRef = useRef<HTMLInputElement>(document.createElement("input"));
+  const passwordConfirmRef = useRef<HTMLInputElement>(document.createElement("input"));
+  const { signup } = useAuth();
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError('');
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to create an account.");
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required></Form.Control>
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required></Form.Control>
+            </Form.Group>
+            <Button type="submit" className="w-100 text-center mt-2" disabled={loading}>Log In</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? Sign Up
+      </div>
+    </div>
+  );
+}
