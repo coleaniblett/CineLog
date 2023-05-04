172ebbe8437680e11d3c9164aa9e4f726085f93d
ColeANiblett <coleaniblett@gmail.com>
2023-05-04 13:56:35 -0700

    add header, searchbar components

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Header } from "../Header/Header";
 
 export const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home w-100" >
+      <Header />
     </div>
   );
 }
---
+++ src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Searchbar: React.FC = () => {
+  return (
+    <div>
+      <input></input>
+    </div>
+  );
+}
