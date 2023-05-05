3ec4b98f2d74c910b838b6e50c1db7b36235b985
ColeANiblett <coleaniblett@gmail.com>
2023-05-05 13:53:40 -0700

    adjust colors, padding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,16 +5,16 @@ import { Link, useNavigate } from 'react-router-dom';
 
 export const Header: React.FC = () => {
   return (
-    <div>
+    <div className="pb-3">
       <Navbar>
         <Container>
-          <Navbar.Brand href="#home" className="me-auto"><Link to="/">CineLog</Link></Navbar.Brand>
+          <Navbar.Brand href="#home" className="me-auto"><Link to="/" className="link-warning">CineLog</Link></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav>
-              <Nav.Link href="#home"><Link to="/reviews">Reviews</Link></Nav.Link>
-              <Nav.Link href="#home"><Link to="/saved">Saved</Link></Nav.Link>
-              <Nav.Link href="#home"><Link to="/settings">Settings</Link></Nav.Link>
+              <Nav.Link href="#home"><Link to="/reviews" className="link-warning">Reviews</Link></Nav.Link>
+              <Nav.Link href="#home"><Link to="/saved" className="link-warning">Saved</Link></Nav.Link>
+              <Nav.Link href="#home"><Link to="/settings" className="link-warning">Settings</Link></Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
