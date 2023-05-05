b85751cfbd8f7cb7807a8d2a41b6a4ec2baa33fc
ColeANiblett <coleaniblett@gmail.com>
2023-05-05 13:19:56 -0700

    style searchbar, add placeholder text

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Searchbar: React.FC = () => {
   return (
     <div className="searchbar d-flex">
-      <input className="m-auto form-control form-control-lg"></input>
+      <input className="m-auto form-control form-control-lg w-75 rounded-pill" placeholder="Search for movies"></input>
     </div>
   );
 }
