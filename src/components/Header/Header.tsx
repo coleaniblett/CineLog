5d3a0789992f85dbd124796008dd5b0f46c5956d
ColeANiblett <coleaniblett@gmail.com>
2023-05-05 13:19:12 -0700

    adjust navbar design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown, SplitButton } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Searchbar } from '../Searchbar/Searchbar';
 
 export const Header: React.FC = () => {
@@ -7,25 +7,15 @@ export const Header: React.FC = () => {
     <div>
       <Navbar>
         <Container>
-          <Navbar.Brand href="#home">CineLog</Navbar.Brand>
+          <Navbar.Brand href="#home" className="me-auto">CineLog</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" >
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Movies</Nav.Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Link href="#home">Reviews</Nav.Link>
+              <Nav.Link href="#home">Saved</Nav.Link>
+              <Nav.Link href="#home">Settings</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-          <NavDropdown title="Settings" id="basic-nav-dropdown" className="m-auto" drop={'start'}>
-            <NavDropdown.Item href="#action/3.1">
-              Your Profile
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Reset Password
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Delete Account
-            </NavDropdown.Item>
-          </NavDropdown>
         </Container>
       </Navbar>
       <Searchbar />
