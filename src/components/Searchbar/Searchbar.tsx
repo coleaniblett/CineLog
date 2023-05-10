4b9e475bb69e49fa0275ddf03bef31579b88f2f5
ColeANiblett <coleaniblett@gmail.com>
2023-05-10 13:52:30 -0700

    add TMDB API calls

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { TMDB } from "../../util/TMDB/TMDB";
 
 export const Searchbar: React.FC = () => {
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    console.log(TMDB.getMovies(text));
+  }, [text]);
+
   return (
     <div className="searchbar d-flex">
-      <input className="m-auto form-control form-control-lg w-75 rounded-pill" placeholder="Search for movies"></input>
+      <input 
+        className="m-auto form-control form-control-lg w-75 rounded-pill" 
+        placeholder="Search for movies"
+        onChange={e => setText(e.target.value)}
+      >
+      </input>
     </div>
   );
 }
---
+++ src/util/TMDB/TMDB.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_TMDB_API_KEY
+const apiURL = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`;
+
+
+
+// TODO: add type safety
+export const TMDB = {
+
+  async getMovies(searchTerm: string) {
+    const response = await axios.get(`${apiURL}&query=${searchTerm}`);
+    const data = response.data.results;
+    console.log(data);
+
+    
+  }
+
+};
