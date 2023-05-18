37b28c96640c139818a313d46abbf0e3a17882fe
ColeANiblett <coleaniblett@gmail.com>
2023-05-18 10:55:30 -0700

    refactor getMovies method

--- src/util/TMDB/TMDB.ts
+++ src/util/TMDB/TMDB.ts
@@ -1,25 +1,30 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { MovieType } from '../MovieType';
 
 const apiKey: string | undefined = process.env.REACT_APP_TMDB_API_KEY
 const apiURL: string = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`;
 const apiURLAlt: string = `https://api.themoviedb.org/3/movie/`;
 
+const getMovies = (searchTerm: string): Promise<any> => {
+  return axios.get(`${apiURL}&query=${searchTerm}`)
+    .then(result => {
+      const movies: any[] = result.data.results;
+      return movies
+        .filter((movie: MovieType) => movie.title.startsWith(searchTerm))
+        .map((movie: MovieType) => ({
+          title: movie.title,
+          id: movie.id,
+          posterPath: movie.posterPath
+        }));
+    })
+    .catch(error => {
+      console.error(error);
+    })
+}
 
 // TODO: add type safety
 export const TMDB = {
-
-  async getMovies(searchTerm: string): Promise<MovieType[]> {
-    const response: AxiosResponse = await axios.get(`${apiURL}&query=${searchTerm}`);
-    const data: any[] = response.data.results;
-    return data
-      .filter((movie: MovieType) => movie.title.startsWith(searchTerm))
-      .map((movie: MovieType) => ({
-        title: movie.title,
-        id: movie.id,
-        posterPath: movie.posterPath
-      }));
-  },
+  getMovies,
 
   async getMovie(movieID: number): Promise<any> {
     let response = await axios.get(`${apiURLAlt}${movieID}?api_key=${apiKey}`);
