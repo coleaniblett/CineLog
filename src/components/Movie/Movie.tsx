6a8f1a436df872c078a62d446fa36b89b90244d6
ColeANiblett <coleaniblett@gmail.com>
2023-05-19 13:39:35 -0700

    refactor using React-Bootstrap Card component

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Header } from '../Header/Header';
-import { TMDB } from '../../util/TMDB/TMDB';
+import React from 'react';
 import { MovieButtons } from '../MovieButtons/MovieButtons';
 import { MovieDataType } from '../../util/MovieDataType';
+import { Card } from 'react-bootstrap';
 
 interface Props {
   setMovie: React.Dispatch<React.SetStateAction<MovieDataType | null>>,
@@ -13,18 +12,15 @@ export const Movie: React.FC<Props> = ({ setMovie, movie }: Props) => {
 
   return (
     <div>
-      {movie && <div className="d-flex flex-column justify-content-center">
-        <h1
-          className="display-1 text-center"
-        >
-          {movie.title}
-        </h1>
-        <img 
+      {movie && <Card className="bg-danger p-2 m-4">
+        <Card.Title className="text-center display-2">{movie.title}</Card.Title>
+        <Card.Img 
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={`${movie.title} film poster`}
           className="p-4"
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
         />
-      </div>}
-      <MovieButtons />
+        <MovieButtons />
+      </Card>}
     </div>
   )
 }
