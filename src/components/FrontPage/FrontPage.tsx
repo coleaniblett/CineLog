bfdd15fc53ab3c8968e9ad6ec3ccad2ed2f79bb1
ColeANiblett <coleaniblett@gmail.com>
2023-05-20 19:17:25 -0700

    use MovieDataType objects for carousel

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FillerImage from '../../assets/Everything_Everywhere_All_at_Once.jpg';
 import FillerImage2 from '../../assets/The Whale.jpg';
 import { MovieCarousel } from './MovieCarousel/MovieCarousel';
+import { MovieDataType, getMovieDataType } from '../../util/MovieDataType';
+import { TMDB } from '../../util/TMDB/TMDB';
 
 export const FrontPage: React.FC = () => {
+  const [featuredMovies, setFeaturedMovies] = useState<MovieDataType[]>([]);
 
-  const fillerData = [
-    {
-      name: "Everything Everywhere All At Once",
-      image: FillerImage
-    },
-    {
-      name: "The Whale",
-      image: FillerImage2
-    }
-  ]
+  getMovieDataType(111)
+    .then(response => {
+      setFeaturedMovies(prev => {
+        return [...prev, response];
+      })
+    })
+  
+  getMovieDataType(222)
+  .then(response => {
+    setFeaturedMovies(prev => {
+      return [...prev, response];
+    })
+  })
+
+  getMovieDataType(333)
+  .then(response => {
+    setFeaturedMovies(prev => {
+      return [...prev, response];
+    })
+  })
+
+  getMovieDataType(444)
+  .then(response => {
+    setFeaturedMovies(prev => {
+      return [...prev, response];
+    })
+  })
 
   return (
     <div className="h-100 w-100 d-inline-block bg-danger">
       {/* Featured movies */}
-      <MovieCarousel movies={fillerData} />
+      <MovieCarousel movies={featuredMovies} />
       {/* Saved movies */}
-      <MovieCarousel movies={fillerData} />
+      <MovieCarousel movies={featuredMovies} />
       {/* Reviewed movies */}
-      <MovieCarousel movies={fillerData} />
+      <MovieCarousel movies={featuredMovies} />
     </div>
   );
 }
--- src/components/FrontPage/MovieCarousel/MovieCarousel.tsx
+++ src/components/FrontPage/MovieCarousel/MovieCarousel.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { MovieDataType } from '../../../util/MovieDataType';
 
 type tempMovieType = {
   image: any,
@@ -7,22 +8,22 @@ type tempMovieType = {
 }
 
 interface Props {
-  movies: tempMovieType[]
+  movies: MovieDataType[];
 }
 
 export const MovieCarousel: React.FC<Props> = ({movies}: Props) => {
   return (
-    <Carousel className="featured-movies border-top border-warning">
+    <Carousel className="border-top border-warning">
       {movies.map(movie => {
         return (
-          <Carousel.Item key={movie.name}>
+          <Carousel.Item key={movie.title}>
             <img
               className="d-block w-75 p-3 m-auto"
-              src={movie.image}
-              alt={movie.name}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
             />
             <Carousel.Caption>
-              <h3>{movie.name}</h3>
+              <h3>{movie.title}</h3>
             </Carousel.Caption>
           </Carousel.Item>
         );
